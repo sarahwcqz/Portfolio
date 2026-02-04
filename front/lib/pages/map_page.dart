1c2f24aba86963069f968bde0354702e87dcdd01
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-02-04 13:34:28 +0100

    implementation of start / destination elements, lets go button + updateAddressFromCoords function, openAddressSearch function + RouteRequest class

--- front/lib/pages/map_page.dart
+++ front/lib/pages/map_page.dart
@@ -3,8 +3,40 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 // GPS
 import 'package:geolocator/geolocator.dart';
+// conversion JSON <-> dart object
+import 'dart:convert';
+// http requests
+import 'package:http/http.dart' as http;
 
-//--------------------------------------- MAP WIDGET ----------------------------------
+// others pages
+import 'address_search_page.dart';
+
+
+
+//============================================================================================================================
+//========================================================== CLASSES =========================================================
+//============================================================================================================================
+
+//============================================ ROUTE REQUEST  ==============================================
+class RouteRequest {
+  final double startLat, startLng, destLat, destLng;
+
+  RouteRequest({
+    required this.startLat,
+    required this.startLng,
+    required this.destLat,
+    required this.destLng,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'start_lat': startLat,
+        'start_lng': startLng,
+        'dest_lat': destLat,
+        'dest_lng': destLng,
+      };
+}
+
+//============================================ MAP  ==============================================
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
@@ -13,11 +45,20 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  // --------------------- VAR ----------------------------
   // controller to manipulate map through code
   final MapController _mapController = MapController();
   // Default position (Paris)
   LatLng _currentPosition = const LatLng(48.8566, 2.3522);
+    // POINT DEPART
+  LatLng? _startPoint;
+  String _startAddress = "Position actuelle";
 
+  // DESTINATION
+  LatLng? _destinationPoint;
+  String _destinationAddress = "Choisir la destination";
+
+// --------------------------- INIT ------------------------------
 // when starting, calls determinePosition function
   @override
   void initState() {
@@ -25,10 +66,14 @@ class _MapPageState extends State<MapPage> {
     _determinePosition();
   }
 
-// ======================================= FUNCTIONS ======================================
+// =============================================================================================================================
+// ============================================================ FUNCTIONS ======================================================
+// =============================================================================================================================
 
-  /// Asks for permission + get location
-  Future<void> _determinePosition() async {
+// ======================================================= determinePosition =================================================
+// Asks for permission + get location
+
+Future<void> _determinePosition() async {
   // GPS activation (T/F)
   bool serviceEnabled;
 
@@ -90,6 +135,9 @@ class _MapPageState extends State<MapPage> {
       });
       _mapController.move(_currentPosition, 15.0);
     }
+
+    // pass location to updateAddress to get a string translation (for UI later)
+    await _updateAddressFromCoords(_currentPosition);
   // if no current position available, display msg
   } catch (e) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -100,45 +148,224 @@ class _MapPageState extends State<MapPage> {
   }
 }
 
-// ======================================================= UI ====================================
+// ======================================================= updateAddressFromCoords =================================================
+// translates current location into string to display it readably (reverse geocoding)
+
+  Future<void> _updateAddressFromCoords(LatLng point) async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://nominatim.openstreetmap.org/reverse?lat=${point.latitude}&lon=${point.longitude}&format=json',
+        ),
+        headers: {'User-Agent': 'com.example.front'},
+      );
+
+      final data = jsonDecode(response.body);
+      setState(() {
+        _startPoint = point;
+        _startAddress = data['display_name'] ?? "Position actuelle";
+      });
+    } catch (e) {
+      setState(() {
+        _startPoint = point;
+        _startAddress = "Position actuelle";
+      });
+    }
+  }
+
+  // ======================================================= openAddressSearch =================================================
+  // opens a search address bar, calls address search page, gets what search page returns (PickedLocation),
+  // transforms it to start/dest point + address, and zooms on result (start + dest)
+
+    Future<void> _openAddressSearch({required bool isStart}) async {
+    final result = await Navigator.push<_PickedLocation>(
+      context,
+      MaterialPageRoute(builder: (_) => const AddressSearchPage()),
+    );
+
+    if (result != null) {
+      setState(() {
+        if (isStart) {
+          _startPoint = result.latLng;
+          _startAddress = result.address;
+        } else {
+          _destinationPoint = result.latLng;
+          _destinationAddress = result.address;
+        }
+      });
+      // Centers on result <------------------------ change it later?
+      _mapController.move(result.latLng, 15.0);
+    }
+  }
+
+  // ======================================================= openAddressSearch =================================================
+  // shapes the request that will be sent to back when user presses "let's go" button
+
+    Future<void> _sendRouteRequest() async {
+    // if no start + dest was chosen
+    if (_startPoint == null || _destinationPoint == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Veuillez définir départ et destination")),
+      );
+      return;
+    }
+
+    // request sent to back for routing
+    final request = RouteRequest(
+      startLat: _startPoint!.latitude,
+      startLng: _startPoint!.longitude,
+      destLat: _destinationPoint!.latitude,
+      destLng: _destinationPoint!.longitude,
+    );
+
+    final response = await http.post(
+      Uri.parse('https://ton-backend.com/api/itineraire'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(request.toJson()),
+    );
+
+    // succes / error handling
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      // TODO: traiter la réponse (itinéraire, polyline, instructions)
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Itinéraire reçu du backend")),  // DEBUG
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Erreur serveur")),
+      );
+    }
+  }  
+
+  // ==================================================================================================================
+  // ======================================================= UI =======================================================
+  // ==================================================================================================================
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        //binding controller
-        mapController: _mapController,
-        options: MapOptions(
-          initialCenter: _currentPosition,
-          initialZoom: 13.0,
-        ),
+      body: Stack(
         children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            // identity of project for api call to OSM
-            userAgentPackageName: 'com.example.front',
-          ),
-          MarkerLayer(
-            markers: [
-              // User on the map (blue marker)
-              Marker(
-                point: _currentPosition,
-                width: 60,
-                height: 60,
-                child: const Icon(
-                  Icons.my_location,
-                  color: Colors.blue,
-                  size: 40,
-                ),
+          // ---------------------------------------- FLUTTER MAP ----------------------------------
+          FlutterMap(
+            //binding controller
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter: _currentPosition,
+              initialZoom: 13.0,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                // identity of project for api call to OSM
+                userAgentPackageName: 'com.example.front',
+              ),
+              // ..............................markers.........................
+              MarkerLayer(
+                markers: [
+                  // start
+                  if (_startPoint != null)
+                    Marker(
+                      point: _startPoint!,
+                      width: 60,
+                      height: 60,
+                      child: const Icon(Icons.location_pin, color: Colors.red, size: 40),
+                    ),
+
+                  // destination
+                  if (_destinationPoint != null)
+                    Marker(
+                      point: _destinationPoint!,
+                      width: 60,
+                      height: 60,
+                      child: const Icon(Icons.flag, color: Colors.green, size: 40),
+                    ),
+                  
+                  // current position(user on the map)
+                  Marker(
+                    point: _currentPosition,
+                    width: 60,
+                    height: 60,
+                    child: const Icon(
+                      Icons.my_location,
+                      color: Colors.blue,
+                      size: 40,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
+
+          // =================================== start + dest fields ======================================
+          Positioned(
+            top: 50,
+            left: 16,
+            right: 16,
+            child: Column(
+              children: [
+                // Champ point de départ
+                GestureDetector(
+                  onTap: () => _openAddressSearch(isStart: true),
+                  child: Material(
+                    elevation: 4,
+                    borderRadius: BorderRadius.circular(8),
+                    child: TextFormField(
+                      enabled: false,
+                      decoration: InputDecoration(
+                        hintText: _startAddress,
+                        prefixIcon: const Icon(Icons.trip_origin),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                // Champ destination
+                GestureDetector(
+                  onTap: () => _openAddressSearch(isStart: false),
+                  child: Material(
+                    elevation: 4,
+                    borderRadius: BorderRadius.circular(8),
+                    child: TextFormField(
+                      enabled: false,
+                      decoration: InputDecoration(
+                        hintText: _destinationAddress,
+                        prefixIcon: const Icon(Icons.flag),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
-      // Recenter button
-      floatingActionButton: FloatingActionButton(
-        // calls determinePosition function when clicked
-        onPressed: _determinePosition,
-        child: const Icon(Icons.gps_fixed),
+
+      // ================================= floating buttons ===========================================
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          // .............. recenter button ..........................
+          FloatingActionButton(
+            heroTag: "gps",
+            onPressed: _determinePosition,
+            child: const Icon(Icons.gps_fixed),
+          ),
+          const SizedBox(height: 10),
+          // .............. let's go button ...........................
+          FloatingActionButton(
+            heroTag: "route",
+            onPressed: _sendRouteRequest,
+            child: const Icon(Icons.directions),
+          ),
+        ],
       ),
     );
   }
