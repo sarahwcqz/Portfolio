f7613285deb198693b767473396b5cd14b894a22
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-02-10 15:33:02 +0100

    routing + basic endpoints

--- back/app/api/v1/router.py
+++ back/app/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints.itineraires import router as routes_router
-from app.api.v1.endpoints.signalements import router as reports_router
+from app.api.v1.endpoints.routes import router as routes_router
+from app.api.v1.endpoints.reports import router as reports_router
 
 api_router = APIRouter()
 
