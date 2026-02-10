4156cfdc4c852aad3c4ceb9bf5016bb71f51c437
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-02-10 15:54:51 +0100

    deleted files

--- back/app/api/v1/endpoints/itineraires.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/routes", tags=["routes"])
-
-@router.post("/")
-def calculate_route():
-    return {"status": "success"}
--- back/app/api/v1/endpoints/signalements.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/reports", tags=["reports"])
-
-@router.post("/")
-def create_report():
-    return {"status": "success"}
