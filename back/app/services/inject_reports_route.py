dc48a543eb5a32ebd9b4e0f33c7dfe39b3d32f7d
Sarah Wacquiez <sarah.wacquiez@gmail.com>
2026-02-12 14:28:44 +0100

    fonction to create polygones from points w/ shapely

--- back/app/services/inject_reports_route.py
+++ back/app/services/inject_reports_route.py
@@ -1,4 +1,5 @@
-
+from shapely.geometry import Point
+from typing import List, Tuple
 
 def calculate_bounding_box(
     start_lat: float,
@@ -7,7 +8,7 @@ def calculate_bounding_box(
     dest_lng: float,
     margin: float = 0.05    # 1° lat = 111km => 5km = 0,05°
 ) -> Tuple[float, float, float, float]:
-"""Calculates bounding box with margin of ~ 5km"""
+    """Calculates bounding box with margin of ~ 5km"""
     
     min_lat = min(start_lat, dest_lat) - margin
     max_lat = max(start_lat, dest_lat) + margin
@@ -16,12 +17,19 @@ def calculate_bounding_box(
     
     return min_lat, max_lat, min_lng, max_lng
 
+
+
 def create_circle_polygon(
     lat: float,
     lng: float,
     radius_meters: float,
-    num_points: int = 8
 ) -> List[List[float]]:
     """Converts a point (report) into polygon to send it to ORS in avoid_polygones"""
-    # To do
-    pass
+    point = Point(lng, lat)
+
+    #buffer uses degrees => conversion of meters in degrees
+    radius_degrees = radius_meters / 111000
+    circle = point.buffer(radius_degrees, quad_segs=2)
+    coords = list(circle.exterior.coords)
+
+    return coords
